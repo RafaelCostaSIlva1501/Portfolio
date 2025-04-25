959e6a8aeb4bf9782aa6108d2cda29af4eedb53e
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2025-04-25 13:05:02 -0300

    update

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,25 +3,16 @@ export const projects = [
   {
     img: "thumb-mini-games",
     title: "Desenvolvimento de Jogos",
-    text: "Uma coleção de jogos interativos ccriados para explorar mecânicas inovadoras, aprimorar habilidades e aprofundar os estudos em programação de forma prática e envolvente.",
+    text: "Uma coleção de jogos interativos criados para explorar mecânicas inovadoras, aprimorar habilidades e aprofundar os estudos em programação de forma prática e envolvente.",
     github: "https://github.com/RafaelCostaSIlva1501/Mini-Game",
     view: "https://rafaelcostasilva1501.github.io/Mini-Game/",
   },
 
   // Landing Pages
   {
-    img: "landing-pages",
+    img: "thumb-landing-pages",
     title: "Landing Pages",
     text: "Projetos de landing pages desenvolvidos com foco em design moderno, performance e usabilidade. Cada página é uma oportunidade para testar novas abordagens, refinar técnicas de front-end e criar experiências envolventes para o usuário.",
-    github: "",
-    view: "",
-  },
-
-  // Ferramentas
-  {
-    img: "ferramentas-utilitarios",
-    title: "Ferramentas & Utilitários",
-    text: "Uma coleção de ferramentas desenvolvidas para facilitar tarefas do dia a dia, explorar conceitos de programação e aprimorar a experiência do usuário. De listas de tarefas a calculadoras e gerenciadores financeiros, cada projeto é um experimento prático voltado para a funcionalidade e eficiência.",
     github: "",
     view: "",
   },
